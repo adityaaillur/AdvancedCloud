3ff6d918079a8f8b67b7f1c6d4a33b368e4b9d28
Aditya Illur <adityaillur@Illurs-Mac.local>
2024-05-18 15:13:23 -0400

    person.go added

---
+++ L2/hello_world/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"kj
+
+	"rsc.io/quote"
+)
+
+const PI = 3.14
+const C = 1
+
+func main() {
+    person1 := person{"t","p"}
+    fmt.Println(person1.fullName())
+
+	// fmt.Println(quote.Go())
+	// power := 9000
+	// fmt.Printf("its over %d", power)
+	// fmt.Println()
+	// fmt.Println(quote.Hello())
+	// fmt.Println(add(1, 2))
+	// fmt.Printf("its over %d", power)
+}
+
+// func add(a, b int) (c int) {
+// 	c = a + b
+// 	return c
+// }
+
+// func num(a, b int) (c int, d int) {
+// 	return a, b
+// }
+
+func (p person) fullName {
+    t + p
+}
---
+++ L2/hello_world/person.go
@@ -0,0 +1,10 @@
+package main
+
+type person struct {
+	firstName string
+	lasttName string
+}
+
+func (p person) fullName() string {
+	return
+}
